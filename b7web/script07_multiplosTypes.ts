a46f2a4ba9d038d1731845d9f4323d6ad50e33ac
Cadu <cadumega@outlook.com>
2021-08-27 18:49:19 -0300

    multiplosTypes

--- b7web/script07_multiplosTypes.ts
+++ b7web/script07_multiplosTypes.ts
@@ -1,3 +1,18 @@
-let idade = 90;
+let codigo: string | number = 90;         // pode ser uma string ou number que o typescript ira aceitar
 
-idade ='25
+codigo = document.getElementById('idade').innerHTML;
+
+
+
+//ex2
+function mostrarIdade(idade: string | number) {
+                                                                // console.log(`Minha idade é: ${idade}`);
+    if(typeof idade ==='string') {
+        console.log(idade.toUpperCase());
+    }else {
+        console.log(idade);
+    }
+}
+
+mostrarIdade(90);
+mostrarIdade('90');
